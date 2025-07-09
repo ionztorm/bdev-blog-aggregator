4178bbf469a24b676beeba6b6785022a19f4357d
Leon <coding@leonlonsdale.dev>
2025-07-09 08:20:44 +0100

    feat(commands): create commands module

---
+++ internal/command/command_structs.go
@@ -0,0 +1,34 @@
+package command
+
+import (
+	"fmt"
+	"gator/internal/config"
+)
+
+type State struct {
+	Cfg *config.Config
+}
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+type Commands struct {
+	Handlers map[string]func(*State, Command) error
+}
+
+func (c *Commands) Run(s *State, cmd Command) error {
+	handler, exists := c.Handlers[cmd.Name]
+	if !exists {
+		return fmt.Errorf("unknown command: %s", cmd.Name)
+	}
+	return handler(s, cmd)
+}
+
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	if c.Handlers == nil {
+		c.Handlers = make(map[string]func(*State, Command) error)
+	}
+	c.Handlers[name] = f
+}
