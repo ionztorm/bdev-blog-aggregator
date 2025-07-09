bda15b6d26639c4a6a6a636ccebcf052ffb3f9f1
Leon <coding@leonlonsdale.dev>
2025-07-09 08:21:46 +0100

    feat(commands): register login handler, listen for args

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"gator/internal/command"
 	"gator/internal/config"
+	"os"
 )
 
 func main() {
@@ -12,12 +14,31 @@ func main() {
 		fmt.Println(err)
 	}
 
-	cfg.SetUser("Leon")
-
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println(err)
+	state := &command.State{
+		Cfg: &cfg,
 	}
 
-	fmt.Printf("%v", cfg)
+	cmdRegistry := command.Commands{
+		Handlers: make(map[string]func(*command.State, command.Command) error),
+	}
+
+	cmdRegistry.Register("login", command.HandleLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gator <command> [args...]")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	cmd := command.Command{
+		Name: cmdName,
+		Args: cmdArgs,
+	}
+
+	if err := cmdRegistry.Run(state, cmd); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
