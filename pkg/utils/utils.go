b6cee7f2507ce24c8072eba4b9fb2e90716e94a3
Leon <coding@leonlonsdale.dev>
2025-07-10 15:40:25 +0100

    chore: clean up comments

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -7,7 +7,6 @@ import (
 
 func SafeClose(c io.Closer) {
 	if err := c.Close(); err != nil {
-		// Log the error or handle it as needed
 		fmt.Printf("Warning: error closing resource: %v\n", err)
 	}
 }
