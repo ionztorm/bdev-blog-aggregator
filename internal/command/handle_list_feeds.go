1eae4bce43e37efeff173dcef3f6cfa2222fc3c5
Leon <coding@leonlonsdale.dev>
2025-07-11 14:14:48 +0100

    feat(command): add feeds command

---
+++ internal/command/handle_list_feeds.go
@@ -0,0 +1,25 @@
+package command
+
+import (
+	"context"
+	"fmt"
+	"gator/internal/state"
+)
+
+func init() {
+	registerGlobal("feeds", HandleListFeeds)
+}
+
+func HandleListFeeds(s *state.State, cmd Command) error {
+
+	feeds, err := s.DB.ListFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("unable to retrieve feeds: %w", err)
+	}
+
+	for _, feed := range feeds {
+		fmt.Printf("* Name: %s, URL: %s, User: %s\n", feed.Name, feed.Url, feed.User.String)
+	}
+
+	return nil
+}
