dc5ade5b434bf35c803f04f61fc86394b3132822
Leon <coding@leonlonsdale.dev>
2025-07-11 20:37:13 +0100

    feat(db): add queries to mark feeds as fetched and get the next feed to fetch

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -21,3 +21,21 @@ FROM
 WHERE
     url = $1;
 
+-- name: MarkFeedFetched :exec
+UPDATE
+    feeds
+SET
+    last_fetched_at = $1,
+    updated_at = $2
+WHERE
+    id = $3;
+
+-- name: GetNextFeedToFetch :one
+SELECT
+    *
+FROM
+    feeds
+ORDER BY
+    last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
