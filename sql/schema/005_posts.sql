4c9f6b8b1c67e67c6554dd94565d22525f775d46
Leon <coding@leonlonsdale.dev>
2025-07-12 11:51:10 +0100

    feat(db): add posts table migrations

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE posts (
+    id uuid PRIMARY KEY NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    title text,
+    url text UNIQUE,
+    description text,
+    published_at timestamp,
+    feed_id uuid NOT NULL,
+    CONSTRAINT fk_feed_id FOREIGN KEY (feed_id) REFERENCES feeds (id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
+
